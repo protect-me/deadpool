39918703aac59f385b25deda139fb4db4cf30946
protect-me <pmfwiww@gmail.com>
2021-06-08 23:09:07 +0900

    loadVideo

---
+++ VideoDuration.js
@@ -0,0 +1,22 @@
+function loadVideo(file) {
+  new Promise((resolve, reject) => {
+    try {
+      let video = document.createElement("video");
+      video.preload = "metadata";
+
+      video.onloadedmetadata = function () {
+        resolve(this);
+      };
+
+      video.onerror = function () {
+        reject("Invalid video. Please select a video file.");
+      };
+
+      video.src = window.URL.createObjectURL(file);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+export { loadVideo };
--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("hell");
+// INTRO
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const textOnVideo = intro.querySelector("h1");
+
+// VIDEO DURATION
+const loadVideo = (file) =>
+  new Promise((resolve, reject) => {
+    try {
+      let video = document.createElement("video");
+      video.preload = "metadata";
+
+      video.onloadedmetadata = function () {
+        resolve(this);
+      };
+
+      video.onerror = function () {
+        reject("Invalid video. Please select a video file.");
+      };
+
+      video.src = window.URL.createObjectURL(file);
+    } catch (e) {
+      reject(e);
+    }
+  });
+const vvv = await loadVideo(video);
+console.log(vvv.duration);
+
+// console.log(video.duration);
+
+// const videoDuration = vid
+console.log(video);
+
+// CONTENT
+const content = document.querySelector(".content");
+const textOnContent = content.querySelector("h1");
+
+// SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//
+let scene = new ScrollMagic.Scene({
+  duration: "1000%",
+  // duration: "100%",
+  triggerElement: intro,
+})
+  .addIndicators()
+  .addTo(controller);
