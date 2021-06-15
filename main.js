60ed2dff082c2f4771cf1ef8aaced85db9d34db5
protect-me <pmfwiww@gmail.com>
2021-06-15 15:01:30 +0900

    Feat: add fadeIn & out animation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const intro = document.querySelector(".intro");
 const video = intro.querySelector("video");
 const videoText = intro.querySelector("h1");
+const overlay = intro.querySelector(".overlay");
 
 // CONTENT
 const content = document.querySelector(".content");
@@ -18,24 +19,56 @@ const scene = new ScrollMagic.Scene({
   // viewport에 대해 상대적으로 어느 시점에서 보여줄 건지를 설정
   // 0일 경우 viewport의 최상단, 1일경우 viewport의 최하단
 })
-  .addIndicators() // indicator를 심어 화면 우측에서 start, end, trigger를 볼 수 있음
+  // .addIndicators() // indicator를 심어 화면 우측에서 start, end, trigger를 볼 수 있음
   .setPin(intro) // intro 요소를 고정시킴. duration(10000)이 지난 후에는 따라오지 않음
   .addTo(controller); // Scene Object를 ScrollMaig Controller에 추가함
 
 // Text Animation
-const textAnim = TweenMax.fromTo(videoText, 3, { opacity: 1 }, { opacity: 0 });
+const textScene1 = TweenMax.fromTo(
+  videoText,
+  3,
+  { opacity: 1 },
+  { opacity: 0 }
+);
 // TweenMax를 사용해 videoText에 애니메이션을 추가
 // 무엇을, 몇초동안, 뭘?어떻게?
 // => videoText를, 3초동안, opacity를 1에서 0으로
-
 let scene2 = new ScrollMagic.Scene({
   triggerElement: intro,
   duration: 3000,
   triggerHook: 0,
 })
-  .setTween(textAnim)
+  .setTween(textScene1)
   .addTo(controller);
 // 상단에서 만든 textAnim을 controller에 추가
+
+const textAnim2 = TweenMax.fromTo(
+  videoText,
+  3,
+  { opacity: 0 },
+  { opacity: 1, delay: 7 }
+);
+let textScene2 = new ScrollMagic.Scene({
+  triggerElement: intro,
+  duration: 10000,
+  triggerHook: 0,
+})
+  .setTween(textAnim2)
+  .addTo(controller);
+
+// Fade In & Out
+const videoFadeIn = new TimelineMax();
+videoFadeIn
+  .to(overlay, 1, { opacity: 0 })
+  .to(overlay, 1, { opacity: 1, delay: 8 });
+
+let fadeIn = new ScrollMagic.Scene({
+  triggerElement: intro,
+  duration: 10000,
+  triggerHook: 0,
+})
+  .setTween(videoFadeIn)
+  .addTo(controller);
 
 // Video Animation
 let accelamount = 0.1;
